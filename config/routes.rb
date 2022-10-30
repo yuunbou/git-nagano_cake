5bddeb98e2e315acce9a43492d08183e45e16443
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-10-30 15:04:15 +0000

    [Add]商品ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   }
   namespace :admin do
     resources :genres
+    resources :items
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
