213f339abf15d0ef47fb20487501034f42076de6
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-10-30 15:09:13 +0000

    [Add]itemsコントローラ内追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,22 @@
 class Admin::ItemsController < ApplicationController
+  def new
+  end
+  
+  def index
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  
+  
 end
