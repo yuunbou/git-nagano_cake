429004696335dfe9db4f26a6defe684df5a17200
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-10-30 14:11:16 +0000

    [Add]ジャンル一覧追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,12 +5,21 @@ class Admin::GenresController < ApplicationController
   end
   
   def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to admin_genres_path
   end
   
   def edit
   end
   
   def update
+  end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
   end
   
 end
