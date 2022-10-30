fb158b9dc0f2e2a797a572a44ca1f346049df432
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-10-30 07:23:05 +0000

    [Update]管理者viewファイル変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+    sessions: "admin/sessions"
+  }
   namespace :admin do
     resources :genres
   end
