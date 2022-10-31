2d9ca428b3ba955a3d37911845b2f483a20d8ec0
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-10-31 13:41:18 +0000

    [Add]商品ページコントローラ追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,19 +7,27 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new(item_params)
     @genre = @item.genres
     @item.save
-    redirect_to admin_item_path(@item.id)
+    redirect_to admin_item_path
   end
 
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genre = @item.genres
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genre = @item.genres
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update(item.params)
+    redirect_to admin_item_path(@item.id)
   end
 
   private
