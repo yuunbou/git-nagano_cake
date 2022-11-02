83d45c12adf5f8d461849622a846fa264801d3f5
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-11-02 14:49:10 +0000

    [Add]管理者トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   namespace :admin do
+    root to: "homes#top"
     resources :genres, only:[:index, :create, :edit, :update]
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
     resources :customers, only:[:index, :show, :edit, :update]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
