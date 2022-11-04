dff38c994c4f7a5ad483e2b7697dfa8908d35d7c
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-11-04 16:15:32 +0000

    [Add]item.rb関連付け記述追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
 
   def get_image(width, height)
   unless image.attached?
