ae3a37de48f81cac4a244234cc623850b8dc76b8
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-11-13 10:02:59 +0000

    [Add]バリデーション追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,13 @@ class Customer < ApplicationRecord
   has_many :order, dependent: :destroy
   has_many :address, dependent: :destroy
 
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :email, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+
 end
