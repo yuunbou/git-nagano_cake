82a48806d59c610aefe2aa932d2e5732905d36d5
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-11-15 03:30:56 +0000

    [Change]メールアドレス部分変更

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,7 +12,7 @@ class Customer < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
-  validates :email, presence: true
+  validates :email, uniqueness: true
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :telephone_number, presence: true
