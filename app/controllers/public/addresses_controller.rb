f994bd3935620d05ae81c4a7e41fbdfffc2479dc
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-11-15 03:46:18 +0000

    [Add]配送先画面追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,13 +1,18 @@
 class Public::AddressesController < ApplicationController
   def index
-    @addresses = Address.all
     @address = Address.new
+    @addresses = Address.all
   end
 
   def create
     @address = Address.new(address_params)
-    address.save
-    redirect_to addresses_path
+    @addresses = Address.all
+    @address.customer_id = current_customer.id
+    if @address.save!
+      redirect_to addresses_path
+    else
+      render :index
+    end
   end
 
   def edit
@@ -22,7 +27,7 @@ class Public::AddressesController < ApplicationController
 
   def destroy
     @address = Address.find(params[:id])
-    @address.destroy
+    @address.destroy!
     redirect_to addresses_path
   end
 
