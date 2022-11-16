95b2e34024ff537864362dec2cec8c810250770e
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-11-16 08:57:20 +0000

    [Fix]配送先一覧コントローラー修正

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,8 @@
 class Public::AddressesController < ApplicationController
   def index
     @address = Address.new
-    @addresses = Address.all
+    @addresses = current_customer.addresses
+    
   end
 
   def create
