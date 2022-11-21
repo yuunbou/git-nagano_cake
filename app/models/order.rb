9576fc1bea4e7eea61220a02f8bd35f6aecc37cd
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-11-21 07:49:58 +0000

    [Add]バリデーション追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,13 @@ class Order < ApplicationRecord
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { awaiting_payment: 0, confirm: 1, produce: 2, prepare: 3, shipped: 4 }
+
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+  
+  def total_price
+    item.with_tax_price * amount
+  end
+
 end
